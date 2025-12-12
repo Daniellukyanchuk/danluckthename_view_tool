edfdee95aff477cc70c4a0d98ccb78d871ce717f
Daniellukyanchuk <daniellukyanchuk@gmail.com>
2025-12-12 15:28:02 +0600

    Dev tool initial commit

---
+++ lib/danluckthename_view_tool.rb
@@ -0,0 +1,7 @@
+require "danluckthename_view_tool/version"
+require "danluckthename_view_tool/renderer"
+
+module DanluckthenameViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/danluckthename_view_tool/version.rb
@@ -0,0 +1,3 @@
+module DanluckthenameViewTool
+  VERSION = "0.1.0"
+end
